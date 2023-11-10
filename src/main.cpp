5f8a57dd24c22c24a9f73099339861865d0c1ad6
Ciachociech <bastanator.rex@gmail.com>
2023-11-11 00:28:27 +0100

    fix: append a library from device

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,9 @@
+#include <SFML/Graphics.hpp>
+
 int main() {
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
 	return 0;
 }
