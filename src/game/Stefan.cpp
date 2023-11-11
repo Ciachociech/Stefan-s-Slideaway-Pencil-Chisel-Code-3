94172466127629d48863f5ca1ad82da4242435fc
Mateusz Chojnacki <62449836+Ciachociech@users.noreply.github.com>
2023-11-12 00:02:37 +0100

    feat: improve movement for Stefan (#9)

--- src/game/Stefan.cpp
+++ src/game/Stefan.cpp
@@ -6,39 +6,67 @@
 
 namespace game {
 
-Stefan::Stefan() : texture("assets/sprites/bulonais.png"), sprite() {
+namespace {
+
+constexpr float maxVelocity = 10.f;
+
+}
+
+Stefan::Stefan() : texture("assets/sprites/bulonais.png"), sprite(), velocity() {
 	this->sprite.setTexture(texture.getTexture());
 }
 
 void Stefan::processInput(const std::vector<window::PressedKey>& keyboardInput, const std::vector<window::PressedButton>& joystickInput) {
 	
 	if (std::find(keyboardInput.begin(), keyboardInput.end(), window::PressedKey::down) != keyboardInput.end() || std::find(joystickInput.begin(), joystickInput.end(), window::PressedButton::down) != joystickInput.end()) {
-		this->sprite.move(sf::Vector2f(0, 2));
+		this->accuracy.y = 2;
 	} else if (std::find(keyboardInput.begin(), keyboardInput.end(), window::PressedKey::up) != keyboardInput.end() || std::find(joystickInput.begin(), joystickInput.end(), window::PressedButton::up) != joystickInput.end()) {
-		this->sprite.move(sf::Vector2f(0, -2));
+		this->accuracy.y = -2;
 	}
 	if (std::find(keyboardInput.begin(), keyboardInput.end(), window::PressedKey::left) != keyboardInput.end() || std::find(joystickInput.begin(), joystickInput.end(), window::PressedButton::left) != joystickInput.end()) {
-		this->sprite.move(sf::Vector2f(-2, 0));
+		this->accuracy.x = -2;
 	}
 	else if (std::find(keyboardInput.begin(), keyboardInput.end(), window::PressedKey::right) != keyboardInput.end() || std::find(joystickInput.begin(), joystickInput.end(), window::PressedButton::right) != joystickInput.end()) {
-		this->sprite.move(sf::Vector2f(2, 0));
+		this->accuracy.x = 2;
 	}
 }
 
 void Stefan::update() {
+	this->velocity.x += this->accuracy.x;
+	this->velocity.y += this->accuracy.y;
+
+	this->accuracy.x = 0;
+	this->accuracy.y = 0;
+
+	const float speedRatio = std::sqrt(this->velocity.x * this->velocity.x + this->velocity.y * this->velocity.y) / maxVelocity;
+	if (speedRatio > 1.f) {
+		this->velocity.x /= speedRatio;
+		this->velocity.y /= speedRatio;
+	}
+
+	const sf::Vector2f movement = this->velocity;
+	this->sprite.move(movement);
+
+	this->velocity.x *= 0.9;
+	this->velocity.y *= 0.98;
+
 	const sf::FloatRect movementRect = sf::FloatRect(32, 0, 288, 120);
 	const sf::Vector2f actualPosition = this->sprite.getSprite().getPosition();
 	const sf::Vector2u textureSize = this->sprite.getSprite().getTexture()->getSize();
 	if (actualPosition.x < movementRect.left) {
 		this->sprite.setPosition(sf::Vector2f(movementRect.left, actualPosition.y));
+		this->velocity.x = 0.0;
 	} else if (actualPosition.x > movementRect.left + movementRect.width - textureSize.x) {
 		this->sprite.setPosition(sf::Vector2f(movementRect.left + movementRect.width - textureSize.x, actualPosition.y));
+		this->velocity.x = 0.0;
 	}
 	if (actualPosition.y < movementRect.top) {
 		this->sprite.setPosition(sf::Vector2f(actualPosition.x, movementRect.top));
+		this->velocity.y = 0.0;
 	}
 	else if (actualPosition.y > movementRect.top + movementRect.height - textureSize.y) {
 		this->sprite.setPosition(sf::Vector2f(actualPosition.x, movementRect.top + movementRect.height - textureSize.y));
+		this->velocity.y = 0.0;
 	}
 }
 
