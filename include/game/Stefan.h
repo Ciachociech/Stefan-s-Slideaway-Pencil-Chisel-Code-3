228674151db12593d544a2a8e2ba481df751e62f
Mateusz Chojnacki <62449836+Ciachociech@users.noreply.github.com>
2023-11-11 20:28:58 +0100

    feat: move Stefan properties to separate class (#5)

---
+++ include/game/Stefan.h
@@ -0,0 +1,27 @@
+#ifndef GAME_STEFAN_H_
+#define GAME_STEFAN_H_
+
+#include <SFML/Graphics.hpp>
+
+#include <graphics/Sprite.h>
+#include <graphics/Texture.h>
+#include <window/JoystickInput.h>
+#include <window/KeyboardInput.h>
+
+namespace game {
+
+class Stefan {
+private:
+	graphics::Texture texture;
+	graphics::Sprite sprite;
+public:
+	Stefan();
+
+	void processInput(const std::vector<window::PressedKey>& keyboardInput, const std::vector<window::PressedButton>& joystickInput);
+
+	void render(sf::RenderWindow* window);
+};
+
+}
+
+#endif // !GAME_STEFAN_H_
