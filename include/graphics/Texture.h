85a08dc35d39a91dffdcf5f39281888e6228caec
Ciachociech <bastanator.rex@gmail.com>
2023-11-11 22:42:08 +0100

    feat: load sprites to stage

--- include/graphics/Texture.h
+++ include/graphics/Texture.h
@@ -10,10 +10,12 @@ namespace graphics {
 class Texture {
 private:
 	sf::Texture texture;
+	std::string path;
 public:
 	Texture(std::string path);
 
 	const sf::Texture& getTexture();
+	const std::string& getPath();
 
 	void setSmoothMode(bool smoothMode);
 };
